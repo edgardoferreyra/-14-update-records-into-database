64789e0255e7341efdcc34a4431b5458dba75b62
edgardoferreyra <edgardo.ivan.ferreyra@gmail.com>
2021-01-04 19:33:58 -0300

    first commit.

---
+++ src/org/studyeasy/model/UsersModel.java
@@ -0,0 +1,67 @@
+package org.studyeasy.model;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.studyeasy.entity.User;
+
+public class UsersModel {
+	
+	
+	public List<User> listUsers(DataSource dataSource) {
+		// Step 1: Initialize connection objects
+				List<User> listUsers = new ArrayList<>();
+				Connection connect = null;
+				Statement stmt =null;
+				ResultSet rs = null;
+				
+				try {
+					connect = dataSource.getConnection();
+					
+					// Step 2: Create a SQL statements string
+					String query="Select * from users";
+					stmt = connect.createStatement();
+					
+					// Step 3: Execute SQL query
+					rs = stmt.executeQuery(query);
+					
+					// Step 4: Process the result set
+					while(rs.next()) {
+						listUsers.add(new User(rs.getInt("users_id"), rs.getString("username"), rs.getString("email")));
+						
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return listUsers;
+	}
+
+	public Boolean addUser(DataSource dataSource, User newUser) {
+		Connection connect = null;
+		PreparedStatement pstm = null;
+		try {
+			connect = dataSource.getConnection();
+			String username=newUser.getUsername();
+			String email=newUser.getEmail();
+			String query = "insert into users (username,email) values (?,?)";
+			pstm = connect.prepareStatement(query);
+			pstm.setString(1,username);
+			pstm.setString(2,email);
+			return pstm.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;			
+		}
+		
+	}
+}
